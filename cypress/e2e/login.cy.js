3654d4ebb376279811bc298eadfcf782e42379aa
Wallace Horacio <whss.dev@gmail.com>
2026-07-19 07:22:25 -0300

    User update scenario added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,7 +7,16 @@ describe('Orange HRM Tests', () => {
     password: '[name="password"]',
     loginButton: '.oxd-button',
     dashboardValidation: 'div.orangehrm-dashboard-grid',
-    errorMessage: '.oxd-alert'
+    errorMessage: '.oxd-alert',
+    myInfoButton: 'a[href="/web/index.php/pim/viewMyDetails"]', 
+    firstNameField: '[name="firstName"]',
+    middleNameField: '[name="middleName"]',
+    lastNameField: '[name="lastName"]',
+    genericField: '.oxd-input--active',
+    dateField: '[placeholder="yyyy-dd-mm"]',
+    dateCloseButton: '.--close',
+    dateTodayButton: '.--today',
+    saveButton: '[type="submit"]'
   }
 
   it('Login - Success', () => {
@@ -25,5 +34,29 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.password).type(userData.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.errorMessage).should('be.visible')
+  })
+
+  it.only('User Info Update', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.username).type(userData.userSuccess.username)
+    cy.get(selectorList.password).type(userData.userSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardValidation).should('be.visible')
+    cy.get(selectorList.myInfoButton).click()
+    cy.location('pathname').should('contain', 'pim/viewPersonalDetails')
+    cy.get(selectorList.firstNameField).clear().type('Teste')
+    cy.get(selectorList.middleNameField).clear().type('de')
+    cy.get(selectorList.lastNameField).clear().type('Cypress')
+    cy.get(selectorList.genericField).eq(3).clear().type('Employee')
+    cy.get(selectorList.genericField).eq(4).clear().type('Other')
+    cy.get(selectorList.genericField).eq(5).clear().type('Driver')
+    cy.get(selectorList.dateField).eq(0).click()
+    cy.get(selectorList.dateTodayButton).eq(0).click()
+    cy.get(selectorList.dateField).eq(1).clear().type('1994-18-07')
+    cy.get(selectorList.dateCloseButton).eq(0).click()
+    cy.get(selectorList.saveButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
   })
 })
